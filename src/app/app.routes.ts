727fa4d003ad2b4aecca4a654b6eab242378d312
Rafael Aznar <rafaaznar@gmail.com>
2025-11-17 11:50:27 +0100

    feat: add Home and Blog components with routing
    - Implemented Home component with a welcome message.
    - Created Blog component with a simple header.
    - Updated app routing to include paths for Home and Blog.
    - Added Menu component for navigation with active route highlighting.
    - Integrated Bootstrap and Bootstrap Icons for styling.
    - Created CSS files for Home, Blog, and Menu components.
    - Added unit tests for Home, Blog, and Menu components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/shared/home/home';
+import { PlistBlog } from './component/shared/feature/blog/plist/plist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'blog', component: PlistBlog },
+];
