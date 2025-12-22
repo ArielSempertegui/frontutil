d054257f66e7121e74fc0e3783a122f1c5129914
ANN <anasilvesttttre@gmail.com>
2025-12-22 10:14:28 +0100

    Frontutil - Ana Silvestre (#31)
    * Restricciones a rutas de silvestre añadidas
    
    * modificaciones de fecha y añadido publicar / despublicar
    
    * fuera botón
    
    * añadido publicado / no publicado
    
    * cambios en caracteres
    
    * modificaciones en admin
    
    * Eliminar desde admin modificado
    
    * Restricciones a rutas de silvestre añadidas
    
    * modificaciones de fecha y añadido publicar / despublicar
    
    * fuera botón
    
    * añadido publicado / no publicado
    
    * cambios en caracteres
    
    * modificaciones en admin
    
    * Eliminar desde admin modificado
    
    * feedback al admin que crea, borra, edita
    
    * modificación en rutas silvestre
    
    * vaciar tabla
    
    * url como link y visualización de imágenes donde estaba el icon
    
    * no visualización si no hay publis
    
    * mensajes, visualizaciones, registros, vaciados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -183,11 +183,11 @@ export const routes: Routes = [
   //
   { path: 'silvestre', component: RoutedUserPlistSilvestre },
   { path: 'silvestre/post/:id', component: RoutedUserViewSilvestre },
-  { path: 'silvestre/plist', component: RoutedAdminPlistSilvestre },
-  { path: 'silvestre/view/:id', component: RoutedAdminViewSilvestre },
-  { path: 'silvestre/new', component: RoutedAdminNewSilvestre },
-  { path: 'silvestre/edit/:id', component: RoutedAdminEditSilvestre },
-  { path: 'silvestre/remove/:id', component: RoutedAdminRemoveSilvestre },
+  { path: 'silvestre/plist', component: RoutedAdminPlistSilvestre, canActivate: [AdminGuard] },
+  { path: 'silvestre/view/:id', component: RoutedAdminViewSilvestre, canActivate: [AdminGuard] },
+  { path: 'silvestre/new', component: RoutedAdminNewSilvestre, canActivate: [AdminGuard], canDeactivate: [PendingChangesGuard] },
+  { path: 'silvestre/edit/:id', component: RoutedAdminEditSilvestre, canActivate: [AdminGuard], canDeactivate: [PendingChangesGuard] },
+  { path: 'silvestre/remove/:id', component: RoutedAdminRemoveSilvestre, canActivate: [AdminGuard] },
   // Vladislav Uski
   // public
   { path: 'visitas', component: UskiVisitasPage },
